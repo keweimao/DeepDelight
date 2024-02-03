2c6a30bfc48bd29b1b6064454bc741750688ba58
Jerry <jerryli4533@gmail.com>
2024-02-03 03:19:08 -0500

    update of README file

--- Thread5/nanoGPT-master/data/shakespeare_char/prepare.py
+++ Thread5/nanoGPT-master/data/shakespeare_char/prepare.py
@@ -15,8 +15,8 @@ input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
 if not os.path.exists(input_file_path):
-    # data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    data_url = 'https://www.gutenberg.org/cache/epub/1342/pg1342.txt'
+    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    # data_url = 'https://www.gutenberg.org/cache/epub/1342/pg1342.txt'
     with open(input_file_path, 'w', encoding='utf-8') as f:
         f.write(requests.get(data_url).text)
 
